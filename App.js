da9cc3b77875edaf2ada9ea9199d2556b3158fbd
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2022-12-24 15:38:11 +0530

    Day 1 updated

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement('h1', null, 'Hello!');
+const heading2 = React.createElement('h2', null, 'Hello!');
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render([heading, heading2]);
