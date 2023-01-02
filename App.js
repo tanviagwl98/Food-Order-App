097ac99f806bd471bba056218ff0095b1b6667c4
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-02 13:42:12 +0530

    Day 2

--- App.js
+++ App.js
@@ -1,4 +1,22 @@
-const heading = React.createElement('h1', null, 'Hello!');
-const heading2 = React.createElement('h2', null, 'Hello!');
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render([heading, heading2]);
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const heading = React.createElement(
+    "h1", {
+        id: "title",
+    },
+    "Hello!"
+);
+const heading2 = React.createElement(
+    "h2", {
+        id: "title heading",
+    },
+    "Hello!"
+);
+const container = React.createElement(
+    "div", {
+        id: "container",
+    }, [heading, heading2]
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
