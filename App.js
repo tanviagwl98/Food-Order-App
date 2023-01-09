752114b74fc69397e84656f418be6a529319e0c8
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-09 11:19:53 +0530

    Update and rename src/App.js to App.js

---
+++ App.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ReactDOM, { createRoot } from "react-dom/client";
+import {Header} from './src/Components/Header';
+import Body from './src/Components/Body'
+/**
+     Header
+        - Logo(Title)
+        - Nav Items(Right Side)
+        - Cart
+     Body 
+        - Search bar
+        - RestrauntList
+          - RestaurantCard (many cards)
+              - Image
+              - Name
+              - Rating
+              - Cusines
+     Footer
+      - links
+      - Copyright
+    
+    */
+
+
+//Config Driven UI
+
+
+
+
+const Footer = () => {
+  return <h4>Footer</h4>;
+};
+
+const AppLayout = () => {
+  return (
+    <>
+      <Header />
+      <Body />
+      <Footer />
+    </>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<AppLayout />);
