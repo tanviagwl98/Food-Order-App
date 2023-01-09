8097cd1fd85eeb559612ee8a394b8a798775cc7f
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-09 11:18:16 +0530

    Update RestaurantCard.js

--- src/Components/RestaurantCard.js
+++ src/Components/RestaurantCard.js
@@ -1,1 +1,20 @@
 
+import { IMG_CDN_URL } from "../config";
+
+const RestrauntCard = ({
+  name,
+  cuisines,
+  cloudinaryImageId,
+  lastMileTravelString,
+}) => {
+  return (
+    <div className="card">
+      <img src={IMG_CDN_URL + cloudinaryImageId} />
+      <h2>{name}</h2>
+      <h3>{cuisines.join(", ")}</h3>
+      <h4>{lastMileTravelString} minutes</h4>
+    </div>
+  );
+};
+
+export default RestrauntCard;
