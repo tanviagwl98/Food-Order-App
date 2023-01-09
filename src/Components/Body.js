6bd6ad06887a082afe3cc427cad28f205c4168d4
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-09 11:17:55 +0530

    Update Body.js

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,1 +1,58 @@
 
+import { restaurantList } from "../config";
+import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
+
+// What is state
+// what is React Hooks? - functions,
+// What is useState
+
+function filterData(searchText, restaurants) {
+  const data = restaurants.filter((restaurant) =>
+    restaurant.data.name.includes(searchText)
+  );
+  console.log("filtered", data)
+
+  return data;
+}
+
+const Body = () => {
+  const [restaurants, setRestaurants] = useState(restaurantList);
+  const [searchText, setSearchText] = useState("");
+
+  return (
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search"
+          value={searchText}
+          onChange={(e) => {
+            setSearchText(e.target.value);
+          }}
+        />
+        <button
+          className="search-btn"
+          onClick={() => {
+            //need to filter the data
+            const data = filterData(searchText, restaurants);
+            // update the state - restaurants
+            setRestaurants(data);
+          }}
+        >
+          Search
+        </button>
+      </div>
+      <div className="restaurant-list">
+        {restaurants.map((restaurant) => {
+          return (
+            <RestaurantCard {...restaurant.data} key={restaurant.data.id} />
+          );
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Body;
