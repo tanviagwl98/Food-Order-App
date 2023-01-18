35abba170ff50e073f209c45e76e33771f582f3d
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-18 13:30:03 +0530

    Login Page added

---
+++ src/Components/Login.js
@@ -0,0 +1,64 @@
+import { Formik } from "formik";
+import * as Yup from "yup";
+
+const schema = Yup.object().shape({
+  email: Yup.string()
+    .required("Email is a required field")
+    .email("Invalid email format"),
+  password: Yup.string()
+    .required("Password is a required field")
+    .min(8, "Password must be atleast 8 characters"),
+});
+
+export const Login = () => {
+  return (
+    <>
+      <Formik
+        validationSchema={schema}
+        initialValues={{ email: "", password: "" }}
+        onSubmit={(values) => {
+          alert(JSON.stringify(values));
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        }) => (
+          <div className="Login">
+            <div className="form">
+              <form noValidate onSubmit={handleSubmit}>
+                <span>Login</span>
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Enter your email"
+                  className="form-control inp_text"
+                  id="email"
+                  onChange={handleChange}
+                  value={values.email}
+                  onBlur={handleBlur}
+                />
+                <p className="error">{errors.email && touched.email && errors.email}</p>
+                <input
+                  type="password"
+                  name="password"
+                  placeholder="Enter your password"
+                  className="form-control"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                />
+                <p className="error">{errors.password && touched.password && errors.password}</p>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+          </div>
+        )}
+      </Formik>
+    </>
+  );
+};
