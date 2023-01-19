d916b7800436573ea9d7268824ec0d4efcb3eaf8
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-19 13:11:27 +0530

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Logo from '../assets/img/foodvillalogo.png'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 const Title = () => (
   <a href="/">
     <img
@@ -14,18 +14,20 @@ const Title = () => (
 // Composing Comopnentss
 export const Header = () => {
   const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  const navigate = useNavigate();
   return (
     <div className="header">
       <Title />
       <div className="nav-items">
         <ul>
-          <Link className="spacing" to="/"><li>Home</li></Link>
-          <Link to="/about" className="spacing"><li>About</li></Link>
-          <Link to= "/contact" className="spacing"><li>Contact</li></Link>
-          <Link to="/" className="spacing"><li>Cart</li></Link>
+          <Link className="spacing" to="/home"><li>Home</li></Link>
+          <Link to="/home/about" className="spacing"><li>About</li></Link>
+          <Link to= "/home/contact" className="spacing"><li>Contact</li></Link>
+          <Link to="/home" className="spacing"><li>Cart</li></Link>
         </ul>
       </div>
-      {isLoggedIn ? (<button onClick={() => {setIsLoggedIn(false)}}>Login</button>):(<button onClick={() => {setIsLoggedIn(true)}}>Logout</button>)}
+//       {isLoggedIn ? (<button onClick={() => {setIsLoggedIn(false)}}>Login</button>):(<button onClick={() => {setIsLoggedIn(true)}}>Logout</button>)}
+    <button className='logout-btn' onClick={() => {navigate('/')}}>Logout</button>
     </div>
   );
 };
