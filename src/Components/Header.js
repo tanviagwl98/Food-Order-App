11fd35f792dd9130d10e3871657385dd11dc6790
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-01-28 19:57:02 +0530

    styled header for small devices

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import Logo from '../assets/img/foodvillalogo.png'
-import {Link, useNavigate} from 'react-router-dom'
-import useOnline from '../utils/useOnline';
+import React from "react";
+import Logo from "../assets/img/foodvillalogo.png";
+import { Link, useNavigate } from "react-router-dom";
+import useOnline from "../utils/useOnline";
 
 const Title = () => (
-  <a href="/">
-    <img
-      className="h-28 p-2"
-      alt="logo"
-      src={Logo}
-    />
-  </a>
+  <div className="w-full md:w-auto">
+  <img
+    className="w-full text-center md:text-left"
+    alt="logo"
+    src={Logo}
+  />
+</div>
 );
 
 // Composing Comopnentss
@@ -19,19 +19,37 @@ export const Header = () => {
   const isOnline = useOnline();
   const navigate = useNavigate();
   return (
-    <div className="flex justify-between bg-pink-50 shadow-lg sm:bg-blue-400 md:bg-yellow-200">
-      <Title />
-      <div className="nav-items">
-        <ul className="flex py-10">
-          <Link className="spacing" to="/home"><li className="px-2">Home</li></Link>
-          <Link to="/home/about" className="spacing"><li className="px-2">About</li></Link>
-          <Link to= "/home/contact" className="spacing"><li className="px-2">Contact</li></Link>
-          <Link to="/home" className="spacing"><li className="px-2">Cart</li></Link>
-          <Link to="/home/instamart" className="spacing"><li className="px-2">Instamart</li></Link>
-        </ul>
-      </div>
-      <h1>{isOnline ? "✅" : "🔴"}</h1>
-      <button className='bg-purple-900 hover:bg-gray-500 text-white rounded-md m-2 p-2 h-14 align-middle my-8' onClick={() => {navigate('/')}}>Logout</button>
+    <div className="flex flex-col md:flex-row justify-between items-center bg-pink-50 shadow-lg mx-4 my-2">
+      <Title/>
+      <ul className="md:flex md:mt-0 mt-2">
+        <Link className="spacing" to="/home">
+          <li className="px-2">Home</li>
+        </Link>
+        <Link to="/home/about" className="spacing">
+          <li className="px-2">About</li>
+        </Link>
+        <Link to="/home/contact" className="spacing">
+          <li className="px-2">Contact</li>
+        </Link>
+        <Link to="/home" className="spacing">
+          <li className="px-2">Cart</li>
+        </Link>
+        <Link to="/home/instamart" className="spacing">
+          <li className="px-2">Instamart</li>
+        </Link>
+      </ul>
+      
+      <button
+        className="p-2 m-2 bg-purple-900 hover:bg-gray-500 text-white rounded-md mt-2 md:mt-0 md:flex"
+        onClick={() => {
+          navigate("/");
+        }}
+      >
+        Logout
+      </button>
     </div>
   );
 };
+
+
+/* <h1>{isOnline ? "✅" : "🔴"}</h1> */
