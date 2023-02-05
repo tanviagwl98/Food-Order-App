28bc9f7835c76b7000dac0d621ee2b1d30b7045f
Tanvi Agarwal <45534001+tanviagwl98@users.noreply.github.com>
2023-02-05 16:14:26 +0530

    clearcart logic

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,11 +1,18 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import FoodItems from "./FoodItems";
+import {clearCart} from "../utils/cartSlice";
 
 const Cart = () => {
   const cartItems = useSelector((store) => store.cart.items);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  }
+
   return (
     <div>
       <h1 className="text-3xl font-bold">Cart Items - {cartItems.length}</h1>
+      <button className="p-2 m-2 bg-green-500 font-bold" onClick={() => handleClearCart()}>Clear Cart</button>
       <div className="flex">{cartItems.map((item)=>(
         <FoodItems key={item.id} {...item}/>
       ))}</div> 
